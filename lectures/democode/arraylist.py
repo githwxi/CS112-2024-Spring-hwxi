9b276207c59f841225519b0dccb2bd2e9842da6a
Hongwei <gmhwxi@gmail.com>
2024-04-11 04:33:47 -0400

    Updating: very very minorly

--- lectures/democode/arraylist.py
+++
@@ -1,6 +0,0 @@
-xs = []
-N0 = 200000
-for i in range(N0):
-    xs.insert(0, i)
-print("len(xs) = ", len(xs))
-
--- lectures/democode/stackover.py
+++
@@ -1,6 +0,0 @@
-
-def tally(x):
-    return ((x + tally(x-1)) if (x > 0) else 1);
-
-print("tally(100) = ", tally(100))
-print("tally(1000000) = ", tally(1000000))
